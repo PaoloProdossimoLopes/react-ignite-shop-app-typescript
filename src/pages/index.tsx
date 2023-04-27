26df420c9f78dc45b1d38115c7f7317b309570de
Paolo Prodossimo Lopes <paolo.prodossimo.lopes@gmail.com>
2023-04-27 11:28:41 -0300

    feat: add home

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,44 @@
-export default function Home() {
+import { styled } from "@/styles"
+import { HomeContainer, Product } from "@/styles/pages/home"
+import Image from "next/image"
+
+import camiseta from '../assets/camisetas/png-transparent-blank-t-shirts-white-t-shirt-template-template-t-shirt-apparel-tee-clothing-cotton.png'
+
+const Button = styled('button', {
+  backgroundColor: 'red'
+})
+ 
+export default function Home() { 
   return (
-   <h1>Hello Next</h1>
+   <HomeContainer>
+    
+    <Product>
+      <Image src={camiseta} width={520} height={480} alt=""/>
+
+      <footer>
+        <strong>Camiseta X</strong>
+        <span>R$ 79,90</span>
+      </footer>
+    </Product>
+    <Product>
+      <Image src={camiseta} width={520} height={480} alt=""/>
+
+      <footer>
+        <strong>Camiseta X</strong>
+        <span>R$ 79,90</span>
+      </footer>
+    </Product>
+    <Product>
+      <Image src={camiseta} width={520} height={480} alt=""/>
+
+      <footer>
+        <strong>Camiseta X</strong>
+        <span>R$ 79,90</span>
+      </footer>
+    </Product>
+
+
+   </HomeContainer>
+   
   )
 }
